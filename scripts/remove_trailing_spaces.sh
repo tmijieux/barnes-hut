c526ead50b70fe09cbf8bbec58bd3c6a13bd6561
Thomas Mijieux <thomas.mijieux@hotmail.fr>
2017-02-15 07:40:19 +0100

    trailing space scripts take cpp into account

--- scripts/remove_trailing_spaces.sh
+++ scripts/remove_trailing_spaces.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . \( -name '*.h' -o -name '*.c' \) -print0 | xargs -i -r -0 sed -r -i 's/\s*$//' {}
+find . \( -name '*.h' -o -name '*.c' -o -name '*.[ch]pp' \) -print0 | xargs -i -r -0 sed -r -i 's/\s*$//' {}
